1dde92ec6b53fba8bab32ff88b1f1f1d2a5a4b92
REKHAE JAVALI <rekhajavali22@gmal.com>
2024-01-18 01:46:14 +0530

    blogui

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import {TopBar} from "./Components/topBar/TopBar";
 // import { Home } from "./pages/home/Home";
 // import { Single } from "./pages/single/Single";
-import {Write} from "./pages/write/Write";
+// import {Write} from "./pages/write/Write";
+// import {Login} from "./pages/login/Login";
+// import {Register } from "./pages/register/Register"
+import {Settings} from "./pages/settings/Settings"
 
 
 function App() {
   return (
     <>
       <TopBar />
-      <Write />
+      <Settings />
       
     </>
   );
