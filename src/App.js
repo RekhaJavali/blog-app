4766e8afb303cf49ab944edb654eb75b28b8e455
REKHAE JAVALI <rekhajavali22@gmal.com>
2024-01-18 12:53:36 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React from "react";
 import {TopBar} from "./Components/topBar/TopBar";
-// import { Home } from "./pages/home/Home";
-// import { Single } from "./pages/single/Single";
-// import {Write} from "./pages/write/Write";
-// import {Login} from "./pages/login/Login";
-// import {Register } from "./pages/register/Register"
-import {Settings} from "./pages/settings/Settings"
+import { Home } from "./pages/home/Home";
+import { Single } from "./pages/single/Single";
+import {Write} from "./pages/write/Write";
+import {Login} from "./pages/login/Login";
+import {Register } from "./pages/register/Register";
+import {Settings} from "./pages/settings/Settings";
+import {Status} from "./pages/status/Status";
+
+import { BrowserRouter, Routes,Route, Link} from "react-router-dom";
+// import { Route, Switch } from "react-router";
 
 
 function App() {
+  const user = false;
+
   return (
     <>
-      <TopBar />
-      <Settings />
-      
+      <BrowserRouter >
+        <TopBar />
+        <Routes>
+          
+          {/* <Switch> */}
+            <Route path="/" element={<Home />}></Route>
+            {/* <Route index element={<Home />}></Route> */}
+            <Route path="/posts" element={<Home/>}></Route>
+            <Route path="/register" element={ user? <Home />:<Register />}></Route>
+            <Route path="/login" element={ user ? <Home /> : <Login />}></Route>
+            <Route path="/settings" element={ user ? <Settings /> : <Login />}></Route>
+            <Route path="/post/:id" element={ <Single /> } ></Route>
+            <Route path="/write" element={ user ? <Write /> :<Login /> }></Route>
+            <Route path="/status" element={ <Status/> }></Route>
+
+            <Route path="*" element={<h1>Not found</h1>}></Route>
+          {/* </Switch> */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/status/Status.jsx
@@ -0,0 +1,19 @@
+import react from "react";
+import "./status.css";
+
+export function Status () {
+    return (
+        <>
+        <div className="Status">
+            <h1 >App in progress</h1>
+            <h3>steps</h3>
+            <ul>
+                <li>Routing pages are under src/pages</li>
+                <li>components for pages are under src/components</li>
+                <li>Routing is implemeted</li>
+                <li>Link is used(react-router), and its syle is in index.html (glabl style)</li>
+            </ul>
+        </div>
+        </>
+    );
+}
