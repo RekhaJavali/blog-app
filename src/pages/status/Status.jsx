c665f5b930d0b968c1f1df69679e31f60bda43e1
REKHAE JAVALI <rekhajavali22@gmal.com>
2024-01-20 21:31:18 +0530

    status updated

--- src/pages/status/Status.jsx
+++ src/pages/status/Status.jsx
@@ -16,6 +16,15 @@ export function Status () {
 
             <ul>api and client
                 <li>Created client folder and Moved the react code to client folder</li>
+                <li>Created api folder afor backend code</li>
+                <li>api: all the node code</li> <li>express installation, mongoosefor db</li>
+		        <li>dotenv for hiding sesitive info</li> 
+                <li>create index.js file</li> 
+		        <li>multer to attach images</li>
+                <li>install node js: npm init</li>
+                <li>npm start</li>
+                <li>npm add modemon : to automatically rerun the script after changes. modify scripts under dependencies, start: nodemon index.js</li>
+                <li></li>
             </ul>
         </div>
         </>
